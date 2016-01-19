c1053e1873d8887d18e3630f639da254d16161ef
arhanis <arhanis@users.noreply.github.com>
2016-01-18 23:43:42 -0600

    Update listartists.py

--- listartists.py
+++ listartists.py
@@ -1,4 +1,4 @@
-#This program scrapes the Wikipedia page listing Armenian Artists for a list of their full names.
+# This program scrapes the Wikipedia page listing Armenian Artists for a list of their full names.
 
 import urllib3
 import certifi
@@ -7,11 +7,15 @@ from bs4 import BeautifulSoup
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 wiki="https://en.wikipedia.org/wiki/List_of_Armenian_artists"
 
+# Open the URL. Throws exception if there's a certification error.
 try:
      page = http.urlopen('GET', wiki, preload_content=False)
 except urllib3.exceptions.SSLError as e:
      print (e)
-
+     
+# Here's the scraping.
+# My target, the artist names, were the only links on the page with a comma "," in the link text.
+# The function extracts all links, removing links with None text. It returns a list strings of the artists' names.
 def get_names(opened_page):
      soup = BeautifulSoup(opened_page,'html.parser')
      links = soup.findAll('a')
